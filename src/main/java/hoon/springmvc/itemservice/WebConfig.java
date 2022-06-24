99f49d37f8f3f9923d0a3d1c1d8ec76e632a1c37
hoon <dltjdgnsok1@naver.com>
2022-06-24 19:29:20 +0900

    스프링 인터셉터 적용

--- src/main/java/hoon/springmvc/itemservice/WebConfig.java
+++ src/main/java/hoon/springmvc/itemservice/WebConfig.java
@@ -2,16 +2,33 @@ package hoon.springmvc.itemservice;
 
 import hoon.springmvc.itemservice.web.filter.LogFilter;
 import hoon.springmvc.itemservice.web.filter.LoginCheckFilter;
+import hoon.springmvc.itemservice.web.interceptor.LogInterceptor;
+import hoon.springmvc.itemservice.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
+    }
 
     @Bean
     public FilterRegistrationBean logFilter() {
@@ -26,7 +43,7 @@ public class WebConfig {
         return filterRegistrationBean;
     }
 
-    @Bean
+    //@Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
